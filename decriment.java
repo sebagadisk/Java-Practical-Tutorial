fffc67478e4958df30e9e9d74969a4c8c947efc4
Sebagadis Kahsay <sebexk3@gmail.com>
2022-10-10 17:23:36 +0300

    Create decriment.java

---
+++ decriment.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class JavaOperators {
+
+    public static void main(String args[]) {
+    
+       int x = 5, y = 3;
+      
+      // pre dicrment --x, post decriment x++
+
+       System.out.println("X: " + x--); //
+
+       System.out.println("Y: " + --y); //
+
+       System.out.println("X: " + --x); //
+
+       System.out.println("Y: " + y--); //
+
+       System.out.println("Y: " + --y); //
+
+       System.out.println("X: " + x--); //
+
+       System.out.println("X: " + --x); //
+
+       System.out.println("X: " + --x); //
+
+       System.out.println("X: " + x); //
+
+       System.out.println("Y: " + y); //
+
+    }
+
+}
