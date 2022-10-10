0037571cee0c0288cdef42fae7ee6c3ec9eb9827
Sebagadis Kahsay <sebexk3@gmail.com>
2022-10-10 17:21:48 +0300

    Update Increment.java

--- Increment.java
+++ Increment.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
 
+public class JavaOperators {
+
+    public static void main(String args[]) {
+
+       int x = 5, y = 3;
+
+       //post increment x++, pre increment ++x
+
+       System.out.println("X: " + x++); //5
+
+       System.out.println("Y: "+ ++y); //4
+
+       System.out.println("X: "+  ++x); //7
+
+       System.out.println("Y: "+ y++); //4
+
+       System.out.println("Y: " + ++y); //6
+
+       System.out.println("X: " + x++); //7
+
+       System.out.println("X: " + ++x); //9
+
+       System.out.println("X: " + ++x); // 10
+
+       System.out.println("X: " + x); //10
+
+       System.out.println("Y: " + y); //6
+
+    }
+
+}
