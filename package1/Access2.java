db6b1722521a77114c2a9503c3db8d20ab4a8ed3
Sebagadis Kahsay <sebexk3@gmail.com>
2022-10-10 17:42:36 +0300

    Create Access2.java

---
+++ package1/Access2.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class Access2 {
+
+    public static void main(String args[]){
+        Access1 obj = new Access1();
+        System.out.println(obj.fullName);
+        System.out.println(obj.age);
+        System.out.println(obj.marks);
+    }
+
+}
