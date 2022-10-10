813681332e516f0c2a56095af619561f1a1a55c9
Sebagadis Kahsay <sebexk3@gmail.com>
2022-10-10 16:24:17 +0300

    Update Age.java

--- Age.java
+++ Age.java
@@ -4,6 +4,22 @@ public class Age {
 
     public static void main(String args[]){
 
+        // int boolean char String double float ...
+        int age;
+
+        System.out.println("Please enter your age");
+        System.out.print("=> ");
+        Scanner input = new Scanner(System.in);
+        age = input.nextInt();
+
+        if(age >= 18){
+            System.out.println("Your age must be >= 18 to watch this movie");
+            System.exit(0);
+        }else{
+            System.out.println("Invalid input");
+            System.exit(0);
+        }
+        
     }
 
 }
